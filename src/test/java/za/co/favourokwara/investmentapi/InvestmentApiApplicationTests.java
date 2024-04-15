8b4ec8f7443abcdb305a065a42d5d8d1d06b27f9
Favour Okwara <favourprogrammer@gmail.com>
2024-04-15 11:57:01 +0000

    build: :tada: Initial commit
    Initial spring-boot project with web and h2 dependency

---
+++ src/test/java/za/co/favourokwara/investmentapi/InvestmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.favourokwara.investmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
